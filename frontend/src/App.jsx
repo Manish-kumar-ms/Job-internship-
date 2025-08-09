7158050ee5805aac50eb147c23702ad88e96e24c
Manish kumar <ms5798034@gmail.com>
2025-08-10 04:07:41 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,19 +7,27 @@ import JobDetails from './pages/JobDetails'
 import CreateJob from './pages/CreateJob'
 import EditJob from './pages/EditJob'
 import ApplyJob from './pages/ApplyJob'
+import { UserDataContext } from './context/UserContext'
 
 const App = () => {
+
+   const  {userData, setUserData,loading} = useContext(UserDataContext);
+
+   if (loading) {
+    return <div>Loading...</div>; // Or a spinner
+  }
+
   return (
    <Routes>
-    <Route path="/" element={<Navigate to="/home" />} />
-    <Route path="/login" element={<Login />} />
+    <Route path="/" element={userData ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+    <Route path="/login" element={userData ? <Navigate to="/home" /> : <Login />} />
 
-    <Route path="/home" element={<Home />} />
-    <Route path="/signup" element={<Signup />} />
-    <Route path="/createjob" element={<CreateJob />} />
-    <Route path="/editjob/:id" element={<EditJob />} />
-    <Route path="/details/:id" element={<JobDetails />} />
-    <Route path="/applyjob/:id" element={<ApplyJob />} />
+    <Route path="/home" element={ userData ? <Home /> : <Navigate to="/login" />} />
+    <Route path="/signup" element={userData ? <Navigate to="/home" /> : <Signup />} />
+    <Route path="/createjob" element={userData ? <CreateJob /> : <Navigate to="/login" />} />
+    <Route path="/editjob/:id" element={userData ? <EditJob /> : <Navigate to="/login" />} />
+    <Route path="/details/:id" element={userData ? <JobDetails /> : <Navigate to="/login" />} />
+    <Route path="/applyjob/:id" element={userData ? <ApplyJob /> : <Navigate to="/login" />} />
     </Routes>
   )
 }
