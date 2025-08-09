f18a5c34a926a6c18970d70b990615251703d744
Manish Kumar <ms5798034@gmail.com>
2025-08-10 04:32:18 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -77,8 +77,8 @@ export const login=async (req,res)=>{
       res.cookie("token",jwtToken, {
             httpOnly: true,
              maxAge: 1*24 * 60 * 60 * 1000, // 1 day
-            sameSite: 'strict',
-            secure:false
+            sameSite: "None",
+            secure: true,  
       })
 
       return res.status(201).json({
