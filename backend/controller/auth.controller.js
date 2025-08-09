6cf75207d768d9b3cf94241e2acd60dddf4fbc1f
Manish Kumar <ms5798034@gmail.com>
2025-08-10 04:41:38 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -97,7 +97,11 @@ export const login=async (req,res)=>{
 
 export const logout=async (req,res)=>{
       try {
-        res.clearCookie("token")
+        res.clearCookie("token", {
+      
+      sameSite: "None",
+      secure: true,
+        });
         return res.status(200).json({ message: "Logout successful" });
       } catch (error) {
         return res.status(500).json({
